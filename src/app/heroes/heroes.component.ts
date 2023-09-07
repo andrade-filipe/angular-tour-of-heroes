e80f3b073a6dc68c9c6c7452898165ff0255c06d
Filipe Andrade <filipeandrade.work@gmail.com>
2023-09-07 14:09:43 -0300

    mensagens foram adicionadas

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,35 @@
-import { Component } from '@angular/core';
-import { HEROES } from '../constants/mock-heroes';
-import { Hero } from '../interfaces/hero';
+import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../interfaces/hero';
+import { HeroService } from '../services/hero-service/hero.service';
+import { MessageService } from '../services/message-service/message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-
-  heroes = HEROES;
+export class HeroesComponent implements OnInit{
+  /* Variables */
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
+  /* Constructor and lifecycle */
+  constructor(private heroService: HeroService,
+    private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  /* Methods */
   onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
