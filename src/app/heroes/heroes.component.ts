8bde1d5a05da127a1348bd7c5a003796b4b983bf
Filipe Andrade <filipeandrade.work@gmail.com>
2023-09-08 14:54:41 -0300

    Search Works and CRUD is complete too

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,9 +6,9 @@ import { MessageService } from '../services/message-service/message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
   /* Variables */
   heroes: Hero[] = [];
 
@@ -29,7 +29,21 @@ export class HeroesComponent implements OnInit{
   // }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
